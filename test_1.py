7033df58bac0523a34031560371a0afb4502dac4
Murad1645 <141578488+Murad1645@users.noreply.github.com>
2023-08-29 10:47:27 +0600

    Update test_1.py

--- test_1.py
+++ test_1.py
@@ -6,12 +6,12 @@ sum_xy=0
 sum_x2=0
 [sum_x:=sum_x+element for element in x]
 [sum_y:=sum_y+element for element in y]
-[sum_x2:=sum_x2+(element*element) for element in x]
+[sum_x2:=sum_x2+(element**2) for element in x]
 
 for i in range(len(x)):
     sum_xy+=x[i]*y[i]
 
-m=((len(x)*sum_xy)-(sum_x*sum_y))/((len(x)*sum_x2)-(sum_x*sum_x))
-c=((sum_y*sum_x2)-(sum_x*sum_xy))/((len(x)*sum_x2)-(sum_x*sum_x))
+m=((len(x)*sum_xy)-(sum_x*sum_y))/((len(x)*sum_x2)-(sum_x**2))
+c=((sum_y*sum_x2)-(sum_x*sum_xy))/((len(x)*sum_x2)-(sum_x**2))
 print(m)
 print(c)
